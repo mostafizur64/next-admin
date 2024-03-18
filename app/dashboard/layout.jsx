ca41872660ed8aaf1be81f0ab46177db939c538b
tarafdar16 <imrantarafdar16@gmail.com>
2024-03-18 23:55:48 +0600

    initial setup

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Sidebar from "../ui/dashboard/sidebar/Sidebar";
+import Navbar from "../ui/dashboard/navbar/Navbar";
+import styles from '@/app/ui/dashboard/dashboard.module.css'
+const layout = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <Sidebar />
+      </div>
+      <div className={styles.content}>
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default layout;
